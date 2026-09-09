f2662dd8d96efa2854b67f11264acc98b0d4261d
yalerikk <331stronger@gmail.com>
2026-09-09 16:20:24 +0300

    initial commit

---
+++ src/test/java/dev/yalerikk/paymentservice/PaymentserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.yalerikk.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
